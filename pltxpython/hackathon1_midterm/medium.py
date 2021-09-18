f4645ae34328db2cf8ff39a7ff4629b52fe64384
Phuong <phuongltx@onemount.com>
2021-09-18 22:04:09 +0700

    up medium

--- pltxpython/hackathon1_midterm/medium.py
+++ pltxpython/hackathon1_midterm/medium.py
@@ -9,6 +9,7 @@ def anagram_number(number):
     new_str = str(number)[::-1]
     new_no = int(new_str)
     print("anagram_number(" + str(number) + ") ==", new_no == number)
+    return new_no == number
 
 # anagram_number(12131213)
 # anagram_number(12134)
@@ -26,14 +27,54 @@ def anagram_number(number):
 # X có thể được đặt trước L (50) và C (100) để tạo ra 40 và 90.
 # C có thể được đặt trước D (500) và M (1000) để tạo thành 400 và 900.
 # Cho một chữ số la mã dạng string, hãy viết hàm roman_to_int() chuyển nó thành một số nguyên.
+
+#c1
+roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+         'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
 def roman_to_int(s):
-    pass
+    import re
+    print(re.findall(r'I[XV]|X[LC]|C[DM]|[IVXLCDM]', s))
+    num = sum(roman[key] for key in re.findall(r'I[XV]|X[LC]|C[DM]|[IVXLCDM]', s))
+    print(s,"=",num)
+    return num
 
+# roman_to_int("LVIII")   #58
+# roman_to_int("IX")      #9
+# roman_to_int("MCMXCIV") #1994
 
-roman_to_int("LVIII")
-roman_to_int("IX")
-roman_to_int("MCMXCIV")
+#c2
+# def roman_to_int(s):
+#     translations = {
+#         "I": 1,
+#         "V": 5,
+#         "X": 10,
+#         "L": 50,
+#         "C": 100,
+#         "D": 500,
+#         "M": 1000
+#     }
+#     number = 0
+#     s = s.replace("IV", "IIII").replace("IX", "VIIII")
+#     s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+#     s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+#     for char in s:
+#         number += translations[char]
+#     return number
 
-
-
-
+#c3
+# def roman_to_int(s):
+#     map_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+#     total = 0
+#     s = s.upper()
+#     length = len(s)
+#     for i in range(length):
+#         num = map_roman.get(s[i])
+#         if(i < length - 1):
+#             num_next = map_roman.get(s[i+1])
+#             if(num < num_next):
+#                 total -= num
+#             else:
+#                 total += num
+#         else:
+#             total += num    
+#     return total    
