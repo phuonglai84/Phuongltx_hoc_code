6d0da27d8c472cda1e272fb671ae1991bc190c88
Phuong <phuongltx@onemount.com>
2021-09-18 15:20:54 +0700

    phuongltx lam bai test

--- pltxpython/hackathon1_midterm/easy.py
+++ pltxpython/hackathon1_midterm/easy.py
@@ -1,7 +1,39 @@
 # 1
+# Viết hàm day_diff() nhận vào ngày phải release sản phẩm và ngày đội dev viết xong code. 
+# Tính số ngày mà team test có để test sản phẩm (= release_date - code_complete_day). 
+# Lưu ý, ngày release sản phẩm sẽ ở định dạng 19/12/2021 còn ngày code_complete có định dạng 2021-17-05
 def day_diff(release_date, code_complete_day):
-    pass
+    from datetime import date
+    from datetime import time
+    from datetime import datetime
+    
+    d1 = datetime.strptime(release_date, "%d/%m/%Y")
+    d2 = datetime.strptime(code_complete_day, "%Y-%d-%m")
+    d = (d1-d2).days
+    print("Days to test = ", d, "days")
+    return d
 
 # 2
+# Viết hàm alpha_num() tìm tất cả những từ trong một câu có chứa cả chữ cái và số. Trả ra danh sách các từ như vậy trong một câu.
+# Vd:
+# str1 = "Emma25 is Data scientist50 and AI Expert"
+# alpha_num(str1) == ["Emma25", "scientist50"]
 def alpha_num(sentence):
-    pass
+    # c1 làm bài này    
+    # result = []
+    # values = sentence.split()
+    # print(values)
+    # for i in values:
+    #     check = i.isalnum() and not i.isalpha() and not i.isdigit()
+    #     if check:
+    #         result.append(i)
+    # print(result)
+    # c2 làm bài này
+    import re
+    matched = re.findall(r'(?:[a-zA-Z]+\d+|\d+[a-zA-Z]+)[a-zA-Z\d]*(?=\s|$)', sentence)
+    print(matched)
+    return matched
+    # return result
+
+# day_diff("19/12/2021", "2021-17-09")
+# alpha_num("Emma25 is Data scientist50 and AI Expert 84pltx 45xxx67 ab3de5f")
--- pltxpython/hackathon1_midterm/medium.py
+++ pltxpython/hackathon1_midterm/medium.py
@@ -1,5 +1,39 @@
+#3
+# Viết hàm anagram_number() 
+# có đầu vào là một số nguyên 
+# và trả ra True nếu sau khi đảo ngược thứ tự các chữ số của số đó vẫn cho ta số ban đầu. 
+# Trả ra False nếu không giống.
+# vd: anagram_number(121121) == True
+#     anagram_number(1254) == False
 def anagram_number(number):
-    pass
+    new_str = str(number)[::-1]
+    new_no = int(new_str)
+    print("anagram_number(" + str(number) + ") ==", new_no == number)
 
+# anagram_number(12131213)
+# anagram_number(12134)
+# anagram_number(8888888888888)
+
+#4
+# Các chữ số La Mã được thể hiện bằng bảy biểu tượng khác nhau: I, V, X, L, C, D và M với
+# I=1; V=5; X=10; L=50; C=100; D=500; M=1000
+
+# Ví dụ: số 2 được viết là II bằng số La Mã, chỉ là hai chữ I được thêm vào với nhau. 12 được viết là XII, đơn giản là X + II. Con số 27 được viết là XXVII, là XX + V + II.
+
+# Chữ số La mã thường được viết từ lớn nhất đến nhỏ nhất từ trái sang phải. Tuy nhiên, số 4 không viết là IIII mà được viết là IV. Bởi vì I đứng trước V, chúng ta lấy 5 - 1 = 4. Nguyên tắc tương tự cũng áp dụng cho số chín, được viết là IX. Có sáu trường hợp phép trừ được sử dụng:
+
+# I có thể được đặt trước V (5) và X (10) để tạo thành 4 và 9.
+# X có thể được đặt trước L (50) và C (100) để tạo ra 40 và 90.
+# C có thể được đặt trước D (500) và M (1000) để tạo thành 400 và 900.
+# Cho một chữ số la mã dạng string, hãy viết hàm roman_to_int() chuyển nó thành một số nguyên.
 def roman_to_int(s):
     pass
+
+
+roman_to_int("LVIII")
+roman_to_int("IX")
+roman_to_int("MCMXCIV")
+
+
+
+
